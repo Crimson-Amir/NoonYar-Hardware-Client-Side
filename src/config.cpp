34f78f73089378a56cef510e3ddb150a01123cb6
amir <ytchanneltinos@gmail.com>
2025-12-13 21:25:36 +0330

    update-code

--- src/config.cpp
+++ src/config.cpp
@@ -25,8 +25,7 @@ unsigned long lastConnectivityCheck = 0;
 unsigned long timeForReceiveBread = 0;
 unsigned long waitDeadline = 0;
 
-uint8_t displayEspNowMac[6] = {0x88, 0x57, 0x21, 0x70, 0x7F, 0x24};
-
+uint8_t displayEspNowMac[6] = {0x00, 0x4B, 0x12, 0x33, 0x69, 0xF4};
 int bread_count = 0;
 int bread_buffer_count = 0;
 int bread_buffer[MAX_KEYS];
